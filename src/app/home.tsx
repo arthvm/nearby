68c795e7233d517da24e5d3eb722da5c603a0649
Arthur Mariano <90999445+arthvm@users.noreply.github.com>
2024-12-10 19:08:33 -0300

    feat(home): add places list on the page

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,11 +1,17 @@
 import { Categories, type CategoriesProps } from "@/components/categories";
+import type { PlaceProps } from "@/components/place";
+import { Places } from "@/components/places";
 import { api } from "@/services/api";
 import { useEffect, useState } from "react";
 import { Alert, View } from "react-native";
 
+type MarketsProps = PlaceProps;
+
 export default function Home() {
   const [categories, setCategories] = useState<CategoriesProps>([]);
   const [selectedCategory, setSelectedCategory] = useState("");
+
+  const [markets, setMarkets] = useState<MarketsProps[]>([]);
 
   async function fetchCategories() {
     try {
@@ -18,17 +24,37 @@ export default function Home() {
     }
   }
 
+  async function fetchMarkets() {
+    try {
+      if (!selectedCategory) {
+        return;
+      }
+
+      const { data } = await api.get(`/markets/category/${selectedCategory}`);
+      setMarkets(data);
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Locais", "Não foi possível carregar os locais.");
+    }
+  }
+
   useEffect(() => {
     fetchCategories();
   }, []);
 
+  useEffect(() => {
+    fetchMarkets();
+  }, [selectedCategory]);
+
   return (
-    <View style={{ flex: 1 }}>
+    <View style={{ flex: 1, backgroundColor: "#CECECE" }}>
       <Categories
         data={categories}
         onSelect={setSelectedCategory}
         selected={selectedCategory}
       />
+
+      <Places data={markets} />
     </View>
   );
 }
