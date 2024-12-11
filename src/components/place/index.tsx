8a95f0713c07c03e047c5639f9cf357183810e4e
Arthur Mariano <90999445+arthvm@users.noreply.github.com>
2024-12-11 14:04:17 -0300

    fix(home): truncate text on place description to 2 lines

--- src/components/place/index.tsx
+++ src/components/place/index.tsx
@@ -1,41 +1,43 @@
 import { colors } from "@/styles/colors";
 import { IconTicket } from "@tabler/icons-react-native";
 import {
-  Image,
-  Text,
-  TouchableOpacity,
-  type TouchableOpacityProps,
-  View,
+	Image,
+	Text,
+	TouchableOpacity,
+	type TouchableOpacityProps,
+	View,
 } from "react-native";
 import { s } from "./style";
 
 export type PlaceProps = {
-  id: string;
-  name: string;
-  description: string;
-  coupons: number;
-  cover: string;
-  address: string;
+	id: string;
+	name: string;
+	description: string;
+	coupons: number;
+	cover: string;
+	address: string;
 };
 
 type Props = TouchableOpacityProps & {
-  data: PlaceProps;
+	data: PlaceProps;
 };
 
 export function Place({ data, ...props }: Props) {
-  return (
-    <TouchableOpacity style={s.container} {...props}>
-      <Image style={s.image} source={{ uri: data.cover }} />
+	return (
+		<TouchableOpacity style={s.container} {...props}>
+			<Image style={s.image} source={{ uri: data.cover }} />
 
-      <View style={s.content}>
-        <Text style={s.name}>{data.name}</Text>
-        <Text style={s.description}>{data.description}</Text>
+			<View style={s.content}>
+				<Text style={s.name}>{data.name}</Text>
+				<Text style={s.description} numberOfLines={2}>
+					{data.description}
+				</Text>
 
-        <View style={s.footer}>
-          <IconTicket size={16} color={colors.red.base} />
-          <Text style={s.tickets}>{data.coupons} cupons disponíveis</Text>
-        </View>
-      </View>
-    </TouchableOpacity>
-  );
+				<View style={s.footer}>
+					<IconTicket size={16} color={colors.red.base} />
+					<Text style={s.tickets}>{data.coupons} cupons disponíveis</Text>
+				</View>
+			</View>
+		</TouchableOpacity>
+	);
 }
